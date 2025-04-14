42b56bd720564b0b2546e611ae83669dbf39b6ec
24LP9559 24LP9559 <24LP9559@G301-CC-206.local>
2025-04-14 12:29:06 +0800

    bp

---
+++ frontend/src/app/(auth)/login/page.tsx
@@ -0,0 +1,27 @@
+import { ChevronLeft } from "lucide-react";
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function page() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <p>Card Footer</p>
+      </CardFooter>
+    </Card>
+  );
+}
