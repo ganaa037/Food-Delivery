5ea7c1e9dc309a6af590fe0de7870bfafa19ec27
24LP9559 24LP9559 <24LP9559@G301-CC-206.local>
2025-04-21 13:51:51 +0800

    p

--- frontend/src/components/FoodCard.tsx
+++ frontend/src/components/FoodCard.tsx
@@ -1,4 +1,13 @@
 "use client";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 import React, { useEffect, useState } from "react";
 
 import Image from "next/image";
@@ -21,27 +30,37 @@ export const FoodCard = () => {
   return (
     <>
       {data?.map((value: any, index: any) => (
-        <div
-          key={index}
-          className="w-[397px] h-[342px] border p-4 rounded-[20px] gap-5 flex flex-col bg-white"
-        >
-          <img
-            className="w-[365px] h-[210px]"
-            src={value.image}
-            alt="foodimage"
-          />
-          <div className="flex flex-col gap-2">
-            <div className=" flex justify-between gap-2 w-full items-center">
-              <p className="text-[#EF4444] text-6 font-semibold leading-8 h-8">
-                {value.foodName}
+        <Dialog key={index}>
+          <DialogTrigger className="w-[397px] h-[342px] border p-4 rounded-[20px] gap-5 flex flex-col bg-white">
+            <img
+              className="w-[365px] h-[210px]"
+              src={value.image}
+              alt="foodimage"
+            />
+            <div className="flex flex-col gap-2">
+              <div className=" flex justify-between gap-2 w-full items-center">
+                <p className="text-[#EF4444] text-6 font-semibold leading-8 h-8">
+                  {value.foodName}
+                </p>
+                <p>{value.price}</p>
+              </div>
+              <p className="text-[#09090B] text-[14px] font-normal leading-5">
+                {value.ingredients}
               </p>
-              <p>{value.price}</p>
             </div>
-            <p className="text-[#09090B] text-[14px] font-normal leading-5">
-              {value.ingredients}
-            </p>
-          </div>
-        </div>
+          </DialogTrigger>
+
+          <DialogContent className="w-[826px] h-[412px]">
+            <DialogHeader>
+              <DialogTitle>
+                <img src={value.image}></img>
+              </DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. Th
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       ))}
     </>
   );
